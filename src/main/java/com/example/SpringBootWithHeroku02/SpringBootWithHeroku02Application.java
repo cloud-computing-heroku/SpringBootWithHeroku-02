add6e0d40b0bb7eb2e191924627e3599e74243d6
HajPhan <hajphan97@gmail.com>
2019-08-17 21:42:45 +0700

    First commit :smile:

---
+++ src/main/java/com/example/SpringBootWithHeroku02/SpringBootWithHeroku02Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootWithHeroku02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithHeroku02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithHeroku02Application.class, args);
+	}
+
+}
